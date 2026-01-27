010e1a1ad40d14deb922d73d03f86298c6c1eb82
Shahzad Ali <shahzadbrohi860@gmail.com>
2026-01-27 16:22:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(
 @st.cache_resource
 def load_model():
     model_path = os.path.join(os.path.dirname(__file__), "model.pkl")
-    with open(model_path, "rb") as file:
+    with open(model.pkl, "rb") as file:
         model = pickle.load(file)
     return model
 
@@ -72,3 +72,4 @@ if st.button("Predict Performance"):
     except Exception as e:
         st.error("❌ Prediction failed")
         st.error(e)
+
